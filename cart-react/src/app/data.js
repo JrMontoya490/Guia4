f02ef03992aff40f7bfbe148e092bc41f6c0d871
José Montoya <125832010+JrMontoya490@users.noreply.github.com>
2024-02-24 08:57:29 -0600

    guia 4

---
+++ cart-react/src/app/data.js
@@ -0,0 +1,66 @@
+export const data = [
+    {
+        id: 1,
+        title: 'Cien años de soledad',
+        price: 100,
+        urlImage: 'https://images.penguinrandomhouse.com/cover/9780525562443',
+        quantity: 1
+    },
+    {
+        id: 2,
+        title: 'El señor de los anillos(Trilogía)',
+        price: 190,
+        urlImage: 'https://proassetspdlcom.cdnstatics2.com/usuaris/libros/fotos/358/original/portada_pack-trilogia-el-senor-de-los-anillos_j-r-r-tolkien_202206071544.jpg',
+    quantity: 1
+    },
+{
+    id: 3,
+        title: 'Cuentos de Barro',
+            price: 30,
+                urlImage: 'https://www.librosdelaballena.com/wp-content/uploads/2020/05/cuentos-barro-244x300.png',
+    quantity: 1
+},
+{
+    id: 4,
+        title: 'Tierra de Infancia',
+            price: 30,
+                urlImage: 'https://marketsvstg.blob.core.windows.net/marketsv/0011705_tierra-de-infancia_510.jpeg',
+    quantity: 1
+}, {
+    id: 5,
+        title: 'Harry Potter Pack',
+            price: 390,
+                urlImage: 'https://contentv2.tap-commerce.com/cover/large/9789878000473_1.jpg?id_com = 1113',
+    quantity: 1
+},{
+    id: 1,
+    title: 'Los oficios del libro',
+    price: 100,
+    urlImage: 'https://www.librosdelaballena.com/wp-content/uploads/2018/04/oficios.png',
+    quantity: 1
+},{
+    id: 1,
+    title: 'Íntimas',
+    price: 100,
+    urlImage: 'https://www.librosdelaballena.com/wp-content/uploads/2019/04/intimas-1.png',
+    quantity: 1
+},{
+    id: 1,
+    title: 'Cuentos criminales',
+    price: 100,
+    urlImage: 'https://www.librosdelaballena.com/wp-content/uploads/2020/05/cuentos-criminales-244x300.png',
+    quantity: 1
+},{
+    id: 1,
+    title: 'Charlotte Temple',
+    price: 100,
+    urlImage: 'https://www.librosdelaballena.com/wp-content/uploads/2018/04/charlotte.png',
+    quantity: 1
+},{
+    id: 1,
+    title: 'Antología sin título',
+    price: 100,
+    urlImage: 'https://www.librosdelaballena.com/wp-content/uploads/2021/04/antologia_sin_titulo-244x300.png',
+    quantity: 1
+}
+];
